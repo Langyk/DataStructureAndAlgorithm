0abaa40cb0f68d0f96514f90abed63f352f89504
Langyk <yakunLang@163.com>
2020-01-02 23:06:24 +0800

    '中缀表达式转后缀表达式'

--- src/stack/ReversePolandExpressionDemon.java
+++ src/stack/ReversePolandExpressionDemon.java
@@ -1,5 +1,6 @@
 package stack;
 
+import javax.naming.ldap.PagedResultsControl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -11,19 +12,100 @@ import java.util.Stack;
  */
 public class ReversePolandExpressionDemon {
     public static void main(String[] args) {
-        String suffixExpression = "3 4 + 5 * 6 -";
-        int value = getListString(suffixExpression);
+        /**
+         * 中缀表达式转后缀表达式
+         */
+        String inSuffixExpression="1+((2+3)*4)-5";
+        List list=getInSuffixExpression(inSuffixExpression);
+        List<String> ls=inFixExpressionToSuffixExpression(list);
+        System.out.println(ls);
+        int value=calculate(ls);
         System.out.println(value);
+
+//        String suffixExpression = "3 4 + 5 * 6 -";
+//       List<String> list = getListString(suffixExpression);
+//       int value=calculate(list);
+//        System.out.println(value);
 
     }
 
-    public static int getListString(String suffixExpression) {
+    /**
+     * 中缀表达式放入list中的方法
+     */
+    public static List<String> getInSuffixExpression(String suffixExpression){
+        List<String> list=new ArrayList<>();
+        int index=0;
+        String str;
+        char c;
+        do{
+            if((c=suffixExpression.charAt(index))<48||(c=suffixExpression.charAt(index))>57){
+                list.add(""+c);
+                index++;
+            }
+            else {
+                str="";
+                //判断是否为多位数
+                if(index<suffixExpression.length()&&(c=suffixExpression.charAt(index))>48&&(c=suffixExpression.charAt(index))<57){
+                    str+=c;
+                    index++;
+                }
+                list.add(str);
+            }
+
+        }while (index<suffixExpression.length());
+        return list;
+    }
+
+    /**
+     * 中缀表达式转后缀表达式的方法InfixExpressionToSuffixExpression
+     * 1+((2+3)*4-5) ===>1 2 3 + 4 * + 5 -
+     */
+    public static List<String> inFixExpressionToSuffixExpression(List<String> inFixExpression){
+        Stack<String> s1=new Stack<>();
+        List<String > s2=new ArrayList<>();
+
+        for(String item:inFixExpression){
+            //如果是一个数，直接加入S2
+                if(item.matches("\\d+")){
+                s2.add(item);
+                }
+                else if(item.equals("(")){
+                    s1.push(item);
+                }else if(item.equals(")")){
+                    while (!s1.peek().equals("(")){
+                        s2.add(s1.pop());
+                    }
+                    s1.pop();
+                }else {
+                    //当item的优先级小于等于s1栈的运算符，将s1栈的运算符弹出加入到s2中。
+                    while (s1.size()!=0&&Operation.oper(item)<=Operation.oper(s1.peek())){
+                            s2.add(s1.pop());
+                    }
+                    s1.push(item);
+                }
+        }
+        //将栈s1中的数据放入到s2集合中
+        while (s1.size()!=0){
+            s2.add(s1.pop());
+        }
+        return s2;
+    }
+
+    /**
+     *后缀表达式放入list中的方法
+     */
+    public static List<String> getListString(String suffixExpression) {
         //将字符串中的字符存入List数组中
         String[] rpn = suffixExpression.split(" ");
         List<String> list = new ArrayList<>();
         for (int i = 0; i < rpn.length; i++) {
             list.add(rpn[i]);
         }
+
+        return list;
+    }
+
+    public static int calculate(List<String> list){
         Stack<String> stack = new Stack();
         //将List数组中的字符放入栈中进行运算
         for (String element : list) {
@@ -47,9 +129,38 @@ public class ReversePolandExpressionDemon {
                 }
                 stack.push(res + "");
             }
+
         }
-        return Integer.parseInt(stack.pop());
+        return Integer.parseInt(stack.pop()) ;
+
     }
 
 
+}
+class  Operation{
+    private static int ADD=1;
+    private static int SUB=1;
+    private static int MUL=2;
+    private static int DIV=2;
+
+    public static int oper(String ch){
+        int res=0;
+        switch (ch){
+            case "+":
+                res=ADD;
+                break;
+            case "-":
+                res=SUB;
+                break;
+            case "*":
+                res=MUL;
+                break;
+            case "/":
+                res=DIV;
+                break;
+            default:
+                System.out.println("该符号不是运算符");
+        }
+        return res;
+    }
 }
