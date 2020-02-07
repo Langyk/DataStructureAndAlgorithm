59c4f5a9544e44f1179ce1ca98a8f649335226ad
Langyk <yakunLang@163.com>
2020-02-07 12:26:32 +0800

    '文件解压'

--- src/tree/huffmanCode/HuffmanCode.java
+++ src/tree/huffmanCode/HuffmanCode.java
@@ -2,10 +2,7 @@ package tree.huffmanCode;
 
 import sun.rmi.runtime.NewThreadAction;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -27,6 +24,12 @@ public class HuffmanCode {
         String dstFile="d://dst.zip";
         zipFile(srcFile,dstFile);
         System.out.println("压缩文件OK");
+
+        String zipFile="d://src.bmp";
+        String dstFile2="d://dst.zip";
+        unZipFile(zipFile,dstFile);
+        System.out.println("解压成功");
+
         /**
          *  压缩率（40-17）/40=57%
          */
@@ -44,6 +47,49 @@ public class HuffmanCode {
 //        byte[] huffmanCodeBytes=zip(contentBytes,huffmanCodes);
 //        System.out.println(Arrays.toString(huffmanCodeBytes));
 //        System.out.println(huffmanCodeBytes.length);
+
+    }
+    /***************************************文件解压****************************************/
+    //编写一个方法，完成对压缩文件的解压
+
+    /**
+     * @param zipFile 准备解压的文件
+     * @param dstFile  准备解压的路径
+     */
+    public static void unZipFile(String zipFile,String dstFile){
+        //定义文件输入流
+        InputStream is=null;
+        //定义一个对象输入流
+        ObjectInputStream ois=null;
+        //定义文件输出流
+        OutputStream os=null;
+        try {
+            //创建文件输入流
+            is=new FileInputStream(zipFile);
+            //创建一个和is关联的对象输入流
+            ois=new ObjectInputStream(is);
+            //读取byte[]数组，huffmanBytes
+            byte[] huffmanBytes=(byte[])ois.readObject();
+            //读取霍夫曼编码表
+            Map<Byte,String> huffmanCodes=(Map<Byte, String>)ois.readObject();
+
+            //解码
+            byte[] bytes=decode(huffmanCodes,huffmanBytes);
+            //将bytes数组写入到目标文件
+            os=new FileOutputStream(dstFile);
+            //写数据到dstFile文件
+            os.write(bytes);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            try {
+                os.close();
+                ois.close();
+                is.close();
+            }catch (Exception e){
+                System.out.println(e.getMessage() );
+            }
+        }
 
     }
     /*******************************文件压缩*******************************************************/
